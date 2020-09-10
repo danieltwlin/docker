f95452f6b89802f479f92c48f77409baab9413e8
daniel <67772752+danieltwlin@users.noreply.github.com>
2020-09-10 14:45:52 +0800

    Update command.sh

--- command/command.sh
+++ command/command.sh
@@ -2,12 +2,10 @@
 
 # Export
 # 使用 export 指令來輸出我們的容器
-
 docker export  MyWeb > MyWeb_Export.tar
 
 
 #在執行docker容器時可以加如下引數來保證每次docker服務重啟後容器也自動重啟：
-
 docker run --restart=always
 
 #如果已經啟動了則可以使用如下命令：
