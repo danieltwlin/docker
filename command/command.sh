98a5df342b316167a170aa4ecd12efd4491e731d
daniel <67772752+danieltwlin@users.noreply.github.com>
2020-10-12 15:11:46 +0800

    Update command.sh

--- command/command.sh
+++ command/command.sh
@@ -10,3 +10,15 @@ docker run --restart=always
 
 #如果已經啟動了則可以使用如下命令：
 docker update --restart=always CONTAINER_ID
+
+
+# 設定資料卷
+docker run -d -P \
+ --name <container_name>
+ --mount source=<volume_name>,target=<target_path_in_container> \
+ # 或 -v <volume_name>:<target_path_in_container> \
+ <image> \
+ <command>
+
+# 以 -v 或 --mount 掛載的差別
+#  -v 會自動創建資料夾，--mount 會報錯
